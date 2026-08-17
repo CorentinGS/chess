efcc042807613ef1c06849a78b1c8da7737c6aa2
Corentin GS <contact@corentings.dev>
2026-08-17 11:29:59 +0200

    test: cover Board.AttacksFrom pieces

--- attacks_test.go
+++ attacks_test.go
@@ -111,3 +111,55 @@ func TestBoardAttacksFromIgnoresPins(t *testing.T) {
 		}
 	}
 }
+
+func TestBoardAttacksFromRemainingPiecesAndPawnHEdge(t *testing.T) {
+	tests := []struct {
+		name string
+		fen  string
+		from Square
+		want []Square
+	}{
+		{
+			name: "knight",
+			fen:  "4k3/8/8/8/4N3/8/8/4K3 w - - 0 1",
+			from: E4,
+			want: []Square{C3, C5, D2, D6, F2, F6, G3, G5},
+		},
+		{
+			name: "bishop",
+			fen:  "4k3/8/8/8/4B3/8/8/4K3 w - - 0 1",
+			from: E4,
+			want: []Square{A8, B1, B7, C2, C6, D3, D5, F3, F5, G2, G6, H1, H7},
+		},
+		{
+			name: "queen",
+			fen:  "4k3/8/8/8/3Q4/8/8/4K3 w - - 0 1",
+			from: D4,
+			want: []Square{A1, A4, A7, B2, B4, B6, C3, C4, C5, D1, D2, D3, D5, D6, D7, D8, E3, E4, E5, F2, F4, F6, G1, G4, G7, H4, H8},
+		},
+		{
+			name: "king",
+			fen:  "4k3/8/8/8/4K3/8/8/8 w - - 0 1",
+			from: E4,
+			want: []Square{D3, D4, D5, E3, E5, F3, F4, F5},
+		},
+		{
+			name: "white pawn H file",
+			fen:  "4k3/8/8/8/7P/8/8/4K3 w - - 0 1",
+			from: H4,
+			want: []Square{G5},
+		},
+		{
+			name: "black pawn H file",
+			fen:  "4k3/8/8/7p/8/8/8/4K3 b - - 0 1",
+			from: H5,
+			want: []Square{G4},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assertSameSquares(t, mustPosition(test.fen).Board().AttacksFrom(test.from), test.want...)
+		})
+	}
+}
